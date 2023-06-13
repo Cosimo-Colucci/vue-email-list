c2c20efb86789b9d363e555aa6a9391b67ea3133
Cosimo-Colucci <ccoluccilavoro@gmail.com>
2023-06-13 18:07:52 +0200

    added console.log

--- js/script.js
+++ js/script.js
@@ -7,15 +7,17 @@ createApp({
     },
     created() {
         
-            for (let i = 0; i < 10; i++) {
-                axios
-                    .get('https://flynn.boolean.careers/exercises/api/random/mail')
-                    .then( (result) => {
-                    this.mailList.push(result.data.response);
-                    });
-            };
+                for (let i = 0; i < 10; i++) {
+                    axios
+                        .get('https://flynn.boolean.careers/exercises/api/random/mail')
+                        .then( (result) => {
+                        this.mailList.push(result.data.response);
+                        console.log(this.mailList);
+                        console.log(result.data.response)
+                        });
+                };
                         
-            },
+            }
     
 
 }).mount('#app')
